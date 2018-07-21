9fa695f34a37617e2e3aca25309a99eb90171d38
Rohan Shukla <voidrohan@gmail.com>
2018-07-21 17:38:09 +0530

    Boost networking commit

---
+++ network/chat/message.hpp
@@ -0,0 +1,16 @@
+#ifndef _CHAT_HPP_
+#define _CHAT_HPP_
+
+#include <cstdio>
+#include <boost/cstdlib.hpp>
+#include <cstring>
+
+class Chat
+{
+  public:
+  enum { header_length = 4; }
+  enum
+  {
+    max_body_length = 512;
+  }
+}
